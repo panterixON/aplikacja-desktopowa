f3b99115c06375574d8ca66aaa00ec434b0fbdc1
gimpson <112972745+panterixON@users.noreply.github.com>
2022-11-16 10:56:25 +0100

    Update aplikacjabmi.py

--- aplikacjabmi.py
+++ aplikacjabmi.py
@@ -1,7 +1,18 @@
 import tkinter as tk
 root = tk.Tk()
+label = tk.Label(text="Oblicz swoje bmi")
+label = tk.Label(text="Wzrost")
+entry1 = tk.Entry()
+label = tk.Label(text="Waga")
+entry2 = tk.Entry()
+label.pack()
+entry1.pack()
+label.pack()
+entry2.pack()
 
 
 
-root.geometry("600x600")
+root.geometry("300x300")
 root.title("Oblicz swoje bmi")
+root.iconbitmap("")
+root.mainloop()
